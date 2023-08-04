c78423a6bab17660219720f8aeaed45d19e42307
Zhuofu.Jiang <15988841761@163.com>
2023-08-04 09:49:50 +0800

    add skip_gram

--- Chapter4/skip_gram.py
+++ Chapter4/skip_gram.py
@@ -1,22 +1,28 @@
-import numpy as np
-from common.layers import Embedding
+# coding: utf-8
+import sys
+sys.path.append('..')  # 为了引入父目录的文件而进行的设定
+from common.layers import *
 from negative_sampling_layer import NegativeSamplingLoss
 
 
 class SkipGram:
     def __init__(self, vocab_size, hidden_size, window_size, corpus):
         V, H = vocab_size, hidden_size
+        rn = np.random.randn
 
         # 初始化权重
-        W_in = 0.01 * np.random.randn(V, H).astype('f')
-        W_out = 0.01 * np.random.randn(V, H).astype('f')
+        W_in = 0.01 * rn(V, H).astype('f')
+        W_out = 0.01 * rn(V, H).astype('f')
 
         # 生成层
         self.in_layer = Embedding(W_in)
-        self.ns_loss = NegativeSamplingLoss(W_out, corpus, power=0.75, sample_size=5)
+        self.loss_layers = []
+        for i in range(2 * window_size):
+            layer = NegativeSamplingLoss(W_out, corpus, power=0.75, sample_size=5)
+            self.loss_layers.append(layer)
 
         # 将所有的权重和梯度整理到列表中
-        layers = [self.in_layer, self.ns_loss]
+        layers = [self.in_layer] + self.loss_layers
         self.params, self.grads = [], []
         for layer in layers:
             self.params += layer.params
@@ -24,23 +30,18 @@ class SkipGram:
 
         # 将单词的分布式表示设置为成员变量
         self.word_vecs = W_in
-        self.batch_size = None
-        self.context_size = None
-        self.hidden_size = H
 
     def forward(self, contexts, target):
-        batch_size, context_size = contexts.shape
-        self.batch_size = batch_size
-        self.context_size = context_size
         h = self.in_layer.forward(target)
-        h = h.repeat(context_size, axis=0)
-        contexts = contexts.reshape(-1)
-        loss = self.ns_loss.forward(h, contexts)
+
+        loss = 0
+        for i, layer in enumerate(self.loss_layers):
+            loss += layer.forward(h, contexts[:, i])
         return loss
 
     def backward(self, dout=1):
-        # dh的形状为batch_size * context_size hidden_size
-        dh = self.ns_loss.backward(dout)
-        dh = dh.reshape(self.batch_size, self.context_size, self.hidden_size).transpose(0, 2, 1).sum(axis=2)
+        dh = 0
+        for i, layer in enumerate(self.loss_layers):
+            dh += layer.backward(dout)
         self.in_layer.backward(dh)
         return None
